30e4e2b50e08f736e57e248446699327db2c1e73
krishnakantakandar <krishnakantakandar@gamil.com>
2021-08-10 10:23:44 +0530

    api key change

--- src/component/weather/Temp.js
+++ src/component/weather/Temp.js
@@ -8,7 +8,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=0d57df695767e6d1ef6ec938df005a6f`;
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=84f2622ba538e60ec23afb8154b1fcaf`;
       const res = await fetch(url);
       const data = await res.json();
       const { temp, humidity, pressure } = data.main;
